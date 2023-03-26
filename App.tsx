761e1803723bdb43faf81637315664af40804e57
John Eastman <jp.robotics@gmail.com>
2023-03-26 09:16:22 -0600

    playing around/tutorial

--- App.tsx
+++ App.tsx
@@ -1,14 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello, world!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { Component } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +9,35 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 40
+  }
 });
+
+interface AppState {
+  count: number;
+}
+
+export default class App extends Component<{}, AppState> {
+
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  onPress(): void {
+    this.setState({count: this.state.count + 1});
+  }
+
+  render(): JSX.Element {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Count: {this.state.count}</Text>
+        <Button title="Increase Count" onPress={this.onPress.bind(this)}></Button>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+}
