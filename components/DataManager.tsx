52fe297668c58af3ce12b970384e3d34ab276a2a
John Eastman <jp.robotics@gmail.com>
2024-06-08 14:58:03 -0600

    add loading indicator when getting and setting data

--- components/DataManager.tsx
+++ components/DataManager.tsx
@@ -1,10 +1,12 @@
-import { Button, View, Text } from "react-native";
+import { Button, View, Text, ActivityIndicator } from "react-native";
 import { baseURL } from "../env.json";
 import { UpdateLists } from "../data/reducers/app.reducer";
 import { useContext, useReducer } from "react";
 import { AppContext } from "../contexts/app.context";
 import {
     DataManagerState,
+    UpdateAll,
+    UpdateLoading,
     UpdateMessage,
     dataManagerReducer,
 } from "../data/reducers/dataManager.reducer";
@@ -14,7 +16,7 @@ import { ListJSON } from "../types";
 type DataManagerProps = {};
 
 function getState(): DataManagerState {
-    return { message: undefined };
+    return { isLoading: false };
 }
 
 export default function DataManager(props: DataManagerProps): JSX.Element {
@@ -26,11 +28,13 @@ export default function DataManager(props: DataManagerProps): JSX.Element {
 
     const [dataManagerData, dataManagerDispatch] = useReducer(
         dataManagerReducer,
-        { message: undefined }
+        getState()
     );
-    const { message } = dataManagerData;
+    const { message, isLoading } = dataManagerData;
 
     const getData = async () => {
+        dataManagerDispatch(new UpdateLoading(true));
+
         const response = await fetch(`${baseURL}/lists`, {
             method: "GET",
         });
@@ -47,10 +51,15 @@ export default function DataManager(props: DataManagerProps): JSX.Element {
         const lists = jsonToLists(listsJSON);
 
         dispatch(new UpdateLists(lists));
-        dataManagerDispatch(new UpdateMessage("Data retrieved successfully"));
+
+        dataManagerDispatch(
+            new UpdateAll(false, "Data retrieved successfully")
+        );
     };
 
     const saveData = async () => {
+        dataManagerDispatch(new UpdateLoading(true));
+
         const body: ListJSON[] = listsToJSON(lists);
 
         const response = await fetch(`${baseURL}/lists`, {
@@ -64,21 +73,32 @@ export default function DataManager(props: DataManagerProps): JSX.Element {
         const responsedata = await response.json();
 
         const { message } = responsedata as { message: string };
-        dataManagerDispatch(new UpdateMessage(message));
+        dataManagerDispatch(new UpdateAll(false, message));
     };
 
     return (
         <>
             <View style={{ flexDirection: "row", columnGap: 10 }}>
                 <View style={{ flex: 1 }}>
-                    <Button title="Get Data" onPress={getData} />
+                    <Button
+                        title="Get Data"
+                        onPress={getData}
+                        disabled={isLoading}
+                    />
                 </View>
 
                 <View style={{ flex: 1 }}>
-                    <Button title="Save Data" onPress={saveData} />
+                    <Button
+                        title="Save Data"
+                        onPress={saveData}
+                        disabled={isLoading}
+                    />
                 </View>
             </View>
-            <Text style={{ fontSize: 18 }}>{message}</Text>
+            {!dataManagerData.isLoading && (
+                <Text style={{ fontSize: 18 }}>{message}</Text>
+            )}
+            {dataManagerData.isLoading && <ActivityIndicator size="large" />}
         </>
     );
 }
--- data/reducers/dataManager.reducer.ts
+++ data/reducers/dataManager.reducer.ts
@@ -1,6 +1,10 @@
-export type DataManagerType = "UPDATE_MESSAGE";
+export type DataManagerType =
+    | "UPDATE_MESSAGE"
+    | "UPDATE_LOADING"
+    | "UPDATE_ALL";
 
 export type DataManagerState = {
+    isLoading: boolean;
     message?: string;
 };
 
@@ -16,14 +20,44 @@ export class UpdateMessage implements DataManagerAction {
     }
 }
 
+export class UpdateLoading implements DataManagerAction {
+    type: DataManagerType = "UPDATE_LOADING";
+    newLoading: boolean;
+    constructor(newLoading: boolean) {
+        this.newLoading = newLoading;
+    }
+}
+
+export class UpdateAll implements DataManagerAction {
+    type: DataManagerType = "UPDATE_ALL";
+    newLoading: boolean;
+    newMessage?: string;
+    constructor(newLoading: boolean, newMessage?: string) {
+        this.newLoading = newLoading;
+        this.newMessage = newMessage;
+    }
+}
+
 export function dataManagerReducer(
     prevState: DataManagerState,
     action: DataManagerAction
-) {
+): DataManagerState {
+    const { isLoading, message } = prevState;
+
     switch (action.type) {
         case "UPDATE_MESSAGE": {
             const { newMessage } = action as UpdateMessage;
-            return { message: newMessage };
+            return { message: newMessage, isLoading: isLoading };
+        }
+
+        case "UPDATE_LOADING": {
+            const { newLoading } = action as UpdateLoading;
+            return { message: message, isLoading: newLoading };
+        }
+
+        case "UPDATE_ALL": {
+            const { newLoading, newMessage } = action as UpdateAll;
+            return { message: newMessage, isLoading: newLoading };
         }
 
         default: {
