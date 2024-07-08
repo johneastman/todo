0e4afa67e875481b46b3cb37a90a40e33b8e76da
John Eastman <jp.robotics@gmail.com>
2024-07-08 15:13:48 -0600

    refactor actions modal

--- components/CellActionsModal.tsx
+++ components/CellActionsModal.tsx
@@ -2,26 +2,20 @@ import { useContext, useEffect, useState } from "react";
 import CustomModal from "./core/CustomModal";
 import CustomDropdown from "./core/CustomDropdown";
 import { SelectionValue } from "../types";
-import { Button, View } from "react-native";
 import { AppContext } from "../contexts/app.context";
 import {
     ActionsModalVisible,
+    DeleteLists,
     SelectAllLists,
 } from "../data/reducers/app.reducer";
 
 type CellActionsModalProps = {};
 
-type Action = {
-    action: string;
-    subAction: string;
-};
-
 export default function CellActionsModal(
     props: CellActionsModalProps
 ): JSX.Element {
-    const [actions, setActions] = useState<Action[]>([
-        { action: "Action", subAction: "Sub Action" },
-    ]);
+    const [items, setItems] = useState<string>("");
+    const [actions, setActions] = useState<string[]>([]);
 
     const appContext = useContext(AppContext);
     const {
@@ -32,71 +26,45 @@ export default function CellActionsModal(
     } = appContext;
 
     useEffect(() => {
-        setActions([{ action: "Action", subAction: "Sub Action" }]);
+        setItems("");
+        setActions([]);
     }, [props]);
 
     const closeModal = (): void => dispatch(new ActionsModalVisible(false));
 
-    const setSelectedAction = (actionIndex: number, newAction: string): void =>
-        setActions(
-            actions.map((a, i) =>
-                actionIndex === i ? { ...a, action: newAction } : a
-            )
-        );
-
-    const setSelectedSubAction = (
-        actionIndex: number,
-        newSubAction: string
-    ): void =>
-        setActions(
-            actions.map((a, i) =>
-                actionIndex === i ? { ...a, subAction: newSubAction } : a
-            )
-        );
-
     const executeAction = (): void => {
-        for (const { action, subAction } of actions) {
-            switch (action) {
-                case "Select": {
-                    switch (subAction) {
-                        case "All": {
-                            dispatch(new SelectAllLists(true));
-                            break;
-                        }
-                        default:
-                            break;
-                    }
-                    break;
-                }
+        // Select Items
+        if (items === "All") dispatch(new SelectAllLists(true));
+        else if (items === "None") dispatch(new SelectAllLists(false));
 
-                case "Deselect": {
-                    switch (subAction) {
-                        case "All": {
-                            dispatch(new SelectAllLists(false));
-                            break;
-                        }
-                        default:
-                            break;
-                    }
-                    break;
-                }
-
-                default:
-                    break;
-            }
+        // Perform Action
+        for (const action of actions) {
+            if (action === "Delete") dispatch(new DeleteLists());
         }
 
         // Dismiss the actions modal
         dispatch(new ActionsModalVisible(false));
     };
 
-    const actionOptions: SelectionValue<string>[] = [
-        { label: "Select", value: "Select" },
-        { label: "Deselect", value: "Deselect" },
+    const setNewAction = (index: number, newAction: string): void =>
+        setActions(actions.map((a, i) => (i === index ? newAction : a)));
+
+    const selectedItems: SelectionValue<string>[] = [
+        { label: "All", value: "All" },
+        { label: "None", value: "None" },
+        { label: "Complete", value: "Complete" },
+        { label: "Incomplete", value: "Incomplete" },
+        { label: "Locked", value: "Locked" },
+        { label: "Unlocked", value: "Unlocked" },
     ];
 
-    const selectActions: SelectionValue<string>[] = [
-        { label: "All", value: "All" },
+    const itemsAction: SelectionValue<string>[] = [
+        { label: "Complete", value: "Complete" },
+        { label: "Incomplete", value: "Incomplete" },
+        { label: "Lock", value: "Lock" },
+        { label: "Unlock", value: "Unlock" },
+        { label: "Delete", value: "Delete" },
+        { label: "Move", value: "Move" },
     ];
 
     return (
@@ -108,45 +76,27 @@ export default function CellActionsModal(
             negativeActionText="Cancel"
             negativeAction={closeModal}
             altActionText="Add"
-            altAction={() =>
-                setActions([
-                    ...actions,
-                    { action: "Action", subAction: "Sub Action" },
-                ])
-            }
+            altAction={() => setActions([...actions, ""])}
         >
-            {actions.map(({ action, subAction }, index) => (
-                <View key={index} style={{ flexDirection: "row", gap: 10 }}>
-                    <Button
-                        title="Delete"
-                        color="red"
-                        onPress={() =>
-                            setActions(actions.filter((_, i) => i !== index))
-                        }
-                    />
+            <CustomDropdown
+                placeholder="Select items"
+                data={selectedItems}
+                selectedValue={items}
+                setSelectedValue={(newItems: string): void =>
+                    setItems(newItems)
+                }
+            />
 
-                    <View style={{ flex: 1 }}>
-                        <CustomDropdown
-                            placeholder={action}
-                            selectedValue={action}
-                            data={actionOptions}
-                            setSelectedValue={(newAction: string) =>
-                                setSelectedAction(index, newAction)
-                            }
-                        />
-                    </View>
-
-                    <View style={{ flex: 1 }}>
-                        <CustomDropdown
-                            placeholder={subAction}
-                            selectedValue={subAction}
-                            data={selectActions}
-                            setSelectedValue={(newSubAction: string) =>
-                                setSelectedSubAction(index, newSubAction)
-                            }
-                        />
-                    </View>
-                </View>
+            {actions.map((action, index) => (
+                <CustomDropdown
+                    key={index}
+                    placeholder="Select action"
+                    data={itemsAction}
+                    selectedValue={action}
+                    setSelectedValue={(newAction: string) =>
+                        setNewAction(index, newAction)
+                    }
+                />
             ))}
         </CustomModal>
     );
